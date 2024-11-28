3c2bd0801d6486c475f08b88b4f5cb292e1b75b2
Jimmy Bourassa <jbourassa@gmail.com>
2024-11-28 10:19:45 -0600

    Benchmark random hand

--- crible-core/benches/score_hand.rs
+++ crible-core/benches/score_hand.rs
@@ -1,11 +1,22 @@
 use crible_core::*;
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BatchSize, Criterion};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("score_hand", |b| {
         let cards = parse_cards("Th Qh Jh 5h").unwrap();
         let hand = Hand::from_slice(&cards).unwrap();
         b.iter(|| hand.score(black_box(Card::new(Number::T, Suit::S)), true));
+    });
+
+    c.bench_function("score_random_hand", |b| {
+        b.iter_batched(
+            || {
+                let mut deck = Deck::new_shuffled();
+                (deck.draw_hand().unwrap(), deck.draw())
+            },
+            |(hand, starter)| hand.score(starter, true),
+            BatchSize::SmallInput,
+        );
     });
 }
 
